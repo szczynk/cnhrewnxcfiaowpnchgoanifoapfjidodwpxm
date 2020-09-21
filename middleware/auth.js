42b4e42a9e95db290443061c8856ad738312fd1c
szczynk <34181070+szczynk@users.noreply.github.com>
2020-09-21 14:14:51 +0700

    Delete auth.js

--- middleware/auth.js
+++
@@ -1,8 +0,0 @@
-export default function ({ store, redirect }) {
-  let auth = store.state.isAuthenticated
-  console.log('MIDDLEWARE AUTH', auth)
-  // if (!auth) {
-  //   return redirect('/login')
-  // }
-}
-  
